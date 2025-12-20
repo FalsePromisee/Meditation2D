3cf3c6db3cbfb8dfc1741e10e4e5899384138bff
Pavel Halkovsky <pasha.galkovsky22@gmail.com>
2025-12-20 14:00:37 +0200

    Added light
    Added 2d light to scene. Made global light on scene = 0, and added some spot lights that around player

--- Meditation/Assets/_Core/Scripts/Player/MouseController.cs
+++ Meditation/Assets/_Core/Scripts/Player/MouseController.cs
@@ -7,6 +7,7 @@ namespace _Core.Scripts.Player
     public class MouseController : MonoBehaviour
     {
         [SerializeField] private float _mouseMinVelocity = 0.1f;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
         public Vector3 mouseDirection{get; private set;}
         private Vector3 _newMousePosition;
         
@@ -19,6 +20,7 @@ namespace _Core.Scripts.Player
         {
             _camera = Camera.main;
             _mouseCollider = GetComponent<CircleCollider2D>();
+            _spriteRenderer.enabled = false;
         }
         private void Update()
         {
@@ -48,12 +50,14 @@ namespace _Core.Scripts.Player
             
             _isMousePressed = true;
             _mouseCollider.enabled = true;
+            _spriteRenderer.enabled = true;
         }
 
         private void StopReflectObjects() // player release button
         {
             _isMousePressed = false;
             _mouseCollider.enabled = false;
+            _spriteRenderer.enabled = false;
         }
 
         private void ContinueReflectingObjects() // player pressed and holding button
